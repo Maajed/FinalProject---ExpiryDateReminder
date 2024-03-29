490dc6c74df6c7699482d67a726866f24b18ee9d
Maajed <majeeed.kini@gmail.com>
2024-03-29 15:38:19 +0300

    Second Commit

--- app/src/main/java/com/example/finalproject/Login.java
+++ app/src/main/java/com/example/finalproject/Login.java
@@ -1,5 +1,6 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,9 +9,21 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class Login extends AppCompatActivity {
 
-    Button redirectSignup;
+    Button redirectSignup,SignIn_btn;
+
+    TextInputLayout username,password;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +31,18 @@ public class Login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
         redirectSignup = findViewById(R.id.redirectSignup);
+        SignIn_btn = findViewById(R.id.SignIn_btn);
+
+        SignIn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser(view);
+            }
+        });
 
         redirectSignup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,4 +52,101 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+
+    private Boolean validateUsername()
+    {
+        String val = username.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            username.setError("Field cannot be empty");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validatePassword() {
+        String val = password.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            password.setError("Field cannot be empty");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void loginUser(View view) {
+        //Validate Login Info
+        if (!validateUsername() | !validatePassword()) {
+            return;
+        } else {
+
+            isUser();
+        }
+    }
+
+
+
+    private void isUser()
+    {
+        String userEnteredUsername = username.getEditText().getText().toString().trim();
+        String userEnteredPassword = password.getEditText().getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUser = reference.orderByChild("username").equalTo(userEnteredUsername);
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    username.setError(null);
+                    username.setErrorEnabled(false);
+
+                    String passwordFromDB = dataSnapshot.child(userEnteredUsername).child("password").getValue(String.class);
+
+                    if(passwordFromDB.equals(userEnteredPassword)){
+                        username.setError(null);
+                        username.setErrorEnabled(false);
+
+                        String nameFromDB = dataSnapshot.child(userEnteredUsername).child("name").getValue(String.class);
+                        String usernameFromDB = dataSnapshot.child(userEnteredUsername).child("username").getValue(String.class);
+                        String phoneNoFromDB = dataSnapshot.child(userEnteredUsername).child("phoneNo").getValue(String.class);
+                        String emailFromDB = dataSnapshot.child(userEnteredUsername).child("email").getValue(String.class);
+
+                        Intent intent = new Intent(getApplicationContext(), Home.class);
+
+                        intent.putExtra("name",nameFromDB);
+                        intent.putExtra("username",usernameFromDB);
+                        intent.putExtra("email",emailFromDB);
+                        intent.putExtra("phoneNo",phoneNoFromDB);
+                        intent.putExtra("password",passwordFromDB);
+                        startActivity(intent);
+
+                    }
+
+                    else
+                    {
+                        password.setError("Wrong Password");
+                    }
+                }
+                else {
+                    username.setError("No such User exists");
+                    username.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/finalproject/SignUp.java
+++ app/src/main/java/com/example/finalproject/SignUp.java
@@ -7,16 +7,54 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignUp extends AppCompatActivity {
 
-    Button redirectLogin;
+    TextInputLayout regName,regUsername,regEmaiL,regPhoneNo,regPassword;
+
+    Button SignUp_Btn, redirectLogin;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //Hooks
+        regName = findViewById(R.id.reg_Name);
+        regUsername = findViewById(R.id.reg_Username);
+        regEmaiL = findViewById(R.id.reg_Email);
+        regPhoneNo = findViewById(R.id.reg_PhoneNo);
+        regPassword = findViewById(R.id.reg_Password);
+        SignUp_Btn = findViewById(R.id.SignUp_Btn);
+
         redirectLogin = findViewById(R.id.redirectLogin);
+
+
+        SignUp_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email = regEmaiL.getEditText().getText().toString();
+                String phoneNo = regPhoneNo.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+
+
+                UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
+
+
+                reference.child(username).setValue(helperClass);
+            }
+        });
 
 
         redirectLogin.setOnClickListener(new View.OnClickListener() {
