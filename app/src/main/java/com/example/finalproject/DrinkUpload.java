0a63f5037bb93db76055c4d7a9887212c118c316
Maajed <majeeed.kini@gmail.com>
2024-04-01 13:52:01 +0300

    Added the notification reminder on the food category

--- app/src/main/java/com/example/finalproject/DrinkUpload.java
+++ app/src/main/java/com/example/finalproject/DrinkUpload.java
@@ -1,8 +1,16 @@
 package com.example.finalproject;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +18,19 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.DateFormat;
 import java.util.Calendar;
 
 public class DrinkUpload extends AppCompatActivity {
@@ -25,10 +45,23 @@ public class DrinkUpload extends AppCompatActivity {
 
     DatePickerDialog picker;
 
+//    private StorageReference storageRef;
+
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private FirebaseAuth firebaseAuth;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drink_upload);
+        setContentView(R.layout.activity_food_upload);
+
+
+        // Initialize Firebase Storage
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
 
         uploadImage = findViewById(R.id.uploadImage);
         uploadProductId = findViewById(R.id.uploadProductId);
@@ -36,6 +69,14 @@ public class DrinkUpload extends AppCompatActivity {
         uploadDesc = findViewById(R.id.uploadDesc);
         uploadDate = findViewById(R.id.uploadDate);
         saveButton = findViewById(R.id.saveButton);
+
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImageChooser();
+            }
+        });
+
 
         uploadDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,43 +96,82 @@ public class DrinkUpload extends AppCompatActivity {
             }
         });
 
-//        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
-//                new ActivityResultContracts.StartActivityForResult(),
-//                new ActivityResultCallback<ActivityResult>() {
-//                    @Override
-//                    public void onActivityResult(ActivityResult result) {
-//                        if(result.getResultCode() ==Activity.RESULT_OK){
-//                            Intent data = result.getData();
-//                            uri = data.getData();
-//                            uploadImage.setImageURI(uri);
-//
-//                        } else {
-//                            Toast.makeText(FoodUpload.this,"No Image Selected", Toast.LENGTH_SHORT).show();
-//                        }
-//
-//                    }
-//                }
-//        );
-//
-//        uploadImage.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent photoPicker = new Intent(Intent.ACTION_PICK);
-//                photoPicker.setType("image/*");
-//                activityResultLauncher.launch(photoPicker);
-//            }
-//        });
-//
-//        saveButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                saveData();
-//            }
-//        });
-//
-//        public void saveData(){
-//
-//
-//        }
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Get a reference to store file at "photos/<FILENAME>"
+                StorageReference fileRef = storageRef
+                        .child("drinks/" + System.currentTimeMillis() + "-" + uri.getLastPathSegment());
+                UploadTask uploadTask = fileRef.putFile(uri);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle unsuccessful uploads
+                        Toast.makeText(DrinkUpload.this, "Upload failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(DrinkUpload.this, "Upload successful", Toast.LENGTH_SHORT).show();
+                        Uri uriImage = uploadTask.getResult().getUploadSessionUri();
+                        imageURL = uriImage.toString();
+                        uploadData();
+
+                    }
+                });
+
+
+            }
+        });
+
+    }
+
+    public void uploadData(){
+        String title = uploadProductId.getText().toString();
+        String name = uploadProductName.getText().toString();
+        String desc = uploadDesc.getText().toString();
+        String expiry = uploadDate.getText().toString();
+
+
+        DataClass dataClass = new DataClass(title, name, desc, imageURL, expiry);
+        //We are changing the child from title to currentDate,
+        // because we will be updating title as well and it may affect child value.
+        String currentDate = DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+        firebaseAuth = FirebaseAuth.getInstance();
+        String currentUser = firebaseAuth.getCurrentUser().getUid();
+        FirebaseDatabase.getInstance().getReference("Categories").child("Drinks").child(currentUser).child(dataClass.getDataId())
+                .setValue(dataClass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(DrinkUpload.this, "Saved", Toast.LENGTH_SHORT).show();
+//                            finish();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(DrinkUpload.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void openImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uri = data.getData();
+            uploadImage.setImageURI(uri);
+        }
     }
 }
--- app/src/main/java/com/example/finalproject/MedCategory.java
+++ app/src/main/java/com/example/finalproject/MedCategory.java
@@ -2,39 +2,102 @@ package com.example.finalproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MedCategory extends AppCompatActivity {
 
     FloatingActionButton fab;
 
     BottomNavigationView nav_view;
-
     FirebaseAuth mAuth;
+    RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    FirebaseDatabase database;
+    List<String> myList = new ArrayList<>();
+    List<DataClass> finalList = new ArrayList<>();
+    private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_med_category);
-
         nav_view = findViewById(R.id.nav_view);
 
         mAuth = FirebaseAuth.getInstance();
 
         fab = findViewById(R.id.fab);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        database = FirebaseDatabase.getInstance();
+
+        String currentUser = mAuth.getCurrentUser().getUid();
+        String statement = "Categories/Medicine/" + currentUser;
+        databaseReference = database.getReference(statement);
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                finalList.clear();
+                int count = 0;
+                for(DataSnapshot itemSnapshot: snapshot.getChildren()){
+                    myList.clear();
+                    for (DataSnapshot valueSnapshot: itemSnapshot.getChildren()){
+                        myList.add(valueSnapshot.getValue(String.class));
+                    }
+                    DataClass dataClass = new DataClass(myList.get(1), myList.get(3), myList.get(0), myList.get(2), myList.get(4));
+                    finalList.add(dataClass);
+//                    Toast.makeText(FoodCategory.this, "Worked", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(FoodCategory.this, itemSnapshot.getValue(String.class), Toast.LENGTH_SHORT).show();
+//                    Log.e("Naim", "itemSnapShot: " + .getValue(String.class));
+                    Log.d("Naim", "listValue: " + myList.get(count));
+                    count++;
+                }
+                recyclerView.setHasFixedSize(true);
+
+                layoutManager = new LinearLayoutManager(getApplicationContext());
+                recyclerView.setLayoutManager(layoutManager);
+
+                MedAdapter medadapter = new MedAdapter(finalList, getApplicationContext());
+
+                recyclerView.setAdapter(medadapter);
+//                DataClass dataClass = new DataClass(myList.get(1), myList.get(3), myList.get(0), myList.get(2), myList.get(4));
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        Log.d("Naim", "finalList: " + finalList.toString());
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MedCategory.this,FoodUpload.class);
+                Intent intent = new Intent(MedCategory.this,MedUpload.class);
                 startActivity(intent);
             }
         });
@@ -47,7 +110,7 @@ public class MedCategory extends AppCompatActivity {
                     intent = new Intent(MedCategory.this,Home.class);
                 }
                 else if(item.getItemId()== R.id.profile_btn){
-                    intent = new Intent(MedCategory.this,Profile.class);
+                        intent = new Intent(MedCategory.this,Profile.class);
                 }
                 else if(item.getItemId()== R.id.logout_btn){
                     mAuth.signOut();
